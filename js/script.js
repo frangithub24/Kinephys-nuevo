156a1bebede88d57b663ba2840dde569509e2e7e
frangithub24 <franciscoesperon22@hotmail.com>
2024-09-10 23:45:42 -0300

    correcciones

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const navList = document.querySelector('.nav-list');
 
     menuIcon.addEventListener('click', function () {
         navList.classList.toggle('show');
     });
-});
