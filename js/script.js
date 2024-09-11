6030f2078969260243a48a0a4fdf66fd58a13bdf
Agustin Macko <mackoaa@gmail.com>
2024-09-11 14:12:04 -0300

    función abrir y cerrar menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-    const menuIcon = document.querySelector('.menu-icon');
-    const navList = document.querySelector('.nav-list');
+    // const menuIcon = document.querySelector('.menu-icon');
+    // const navList = document.querySelector('.nav-list');
 
-    menuIcon.addEventListener('click', function () {
-        navList.classList.toggle('show');
+
+    
+
+    // menuIcon.addEventListener('click', function () {
+    //     navList.classList.toggle('block');
+    // });
+
+// DOMContentLoaded permite que se cargue todo al iniciar la pagina
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Guardo en las variables del botón hamburguesa y el listado a mostrado
+    const hamburgerButton = document.querySelector('.menu-icon') 
+    const navMobile = document.querySelector('.nav-mobile') 
+
+
+    //Función para abrir y cerrar el menú modificando el estilo 
+    hamburgerButton.addEventListener('click', () => {
+
+        if(navMobile.style.display ==='block') {
+            navMobile.style.display = 'none';
+        } else {
+            navMobile.style.display = 'block'
+        }
+
     });
+
+})
