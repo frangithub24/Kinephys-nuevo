f3bb3697d40249a1a0723bbf8f3dd47363916d1f
frangithub24 <franciscoesperon22@hotmail.com>
2024-09-17 09:52:03 -0300

    17 de septiembre menu hamburguesa y cruz

--- js/script.js
+++ js/script.js
@@ -12,19 +12,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //Guardo en las variables del botón hamburguesa y el listado a mostrado
-    const hamburgerButton = document.querySelector('.menu-icon') 
-    const navMobile = document.querySelector('.nav-mobile') 
-
+    const hamburgerButton = document.querySelector('.menu-icon'); 
+    const navMobile = document.querySelector('.nav-mobile'); 
+    const backButton = document.querySelector(".menu-back");
+    
 
     //Función para abrir y cerrar el menú modificando el estilo 
     hamburgerButton.addEventListener('click', () => {
 
         if(navMobile.style.display ==='block') {
             navMobile.style.display = 'none';
+            backButton.style.display = 'none';
         } else {
-            navMobile.style.display = 'block'
+            navMobile.style.display = 'block';
+            backButton.style.display = 'block';
+            hamburgerButton.style.display ='none';
         }
-
     });
 
-})
+    //Función para volver al menú hamburguesa cuando toco la x
+    backButton.addEventListener("click", () => {
+        navMobile.style.display = 'none';
+        hamburgerButton.style.display = 'block';
+        backButton.style.display = 'none';
+    })
+});
